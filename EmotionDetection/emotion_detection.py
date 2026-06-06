c538e66f6009072235afa8d57e11b7f9618579c5
g4tto <lucagattis@gmail.com>
2026-06-06 08:44:29 +0200

    finalization

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -13,6 +13,15 @@ def emotion_detector(text_to_analyze):
     myobj = {"raw_document": {"text": text_to_analyze}}
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json=myobj, headers=header, timeout=5)
+    if response.status_code == 400:
+        return {
+            "anger": None,
+            "disgust": None,
+            "fear": None,
+            "joy": None,
+            "sadness": None,
+            "dominant_emotion": None
+        }
     formatted_response = json.loads(response.text)
     emotion = formatted_response["emotionPredictions"][0]["emotion"]
     max = 0.0
---
+++ server.py
@@ -0,0 +1,44 @@
+''' to be executed over the Flask channel and deployed on
+    localhost:5000.
+'''
+from flask import Flask, render_template, request
+
+from .EmotionDetection.emotion_detection import emotion_detector
+
+# Initiate the flask app :
+app = Flask("Emotion detector")
+
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    ''' give the result of the eomotion detector
+    '''
+    #
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    anger = response["anger"]
+    disgust = response["disgust"]
+    fear = response["fear"]
+    joy = response["joy"]
+    sadness = response["sadness"]
+    dominant_emotion = response["dominant_emotion"]
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
+
+    return (
+        f"For the given statement, the system response is 'anger': {anger}, 'disgust': {disgust},"
+        f" 'fear': {fear}, 'joy': {joy} and 'sadness': {sadness}. "
+        f"The dominant emotion is <b>{dominant_emotion}</b>")
+
+
+@app.route("/")
+def render_index_page():
+    ''' This function initiates the rendering of the main application
+        page over the Flask channel
+    '''
+
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
